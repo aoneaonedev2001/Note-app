04ab5f7fe635242bbe6b8ae811a98d92adcf41db
AoneDev2001 <aonedev2001@gmail.com>
2024-05-27 17:04:35 +0700

    get cloud free

--- server/db.js
+++ server/db.js
@@ -1,11 +1,15 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
-});
+// 1.connect database in localhost
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_DATABASE,
+// });
+
+//2. connect database in cloud 
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
